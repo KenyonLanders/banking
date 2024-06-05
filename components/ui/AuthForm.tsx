3f1b481c20141dc0baf7142d087886d6d96c0627
Kenyon Landers <kenyon.landerds3@gmail.com>
2024-06-05 10:06:49 +0200

    Implement PlaidLink and Dwolla

--- components/ui/AuthForm.tsx
+++ components/ui/AuthForm.tsx
@@ -22,6 +22,7 @@ import { authFormSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions';
+import PlaidLink from './PlaidLink';
 
 const AuthForm = ({ type }: { type: string }) => {
     const router = useRouter();
@@ -46,13 +47,27 @@ const AuthForm = ({ type }: { type: string }) => {
     try {
         // Sign up with Appwrite & create plaid link token
 
-        if(type === 'sign-up'){
-            const newUser = await signUp(data);
+        if(type === 'sign-up') {
+
+        const userData = {
+                firstName: data.firstName!,
+                lastName: data.lastName!,
+                address1: data.address1!,
+                city: data.city!,
+                state: data.state!,
+                postalCode: data.postalCode!,
+                dateOfBirth: data.dateOfBirth!,
+                ssn: data.ssn!,
+                email: data.email,
+                password: data.password
+            }
+
+            const newUser = await signUp(userData);
 
             setUser(newUser);
         }
 
-        if (type === 'sign-in'){
+        if (type === 'sign-in') {
             const response = await signIn({
                 email: data.email,
                 password: data.password,
@@ -100,7 +115,7 @@ const AuthForm = ({ type }: { type: string }) => {
         </header>
         {user ? (
             <div className="flex flex-col gap-4">
-                {/* PlaidLink */}
+                <PlaidLink user={user} variant="primary" />
             </div>
         ): (
           <>
@@ -117,13 +132,13 @@ const AuthForm = ({ type }: { type: string }) => {
                                     <CustomInput control={form.control} name="city" label="City" placeholder="Enter your city" />
 
                                     <div className="flex gap-4">
-                                        <CustomInput control={form.control} name="state" label="State/Province" placeholder="Example: NY/Gauteng" />
-                                        <CustomInput control={form.control} name="postalCode" label="Postal Code" placeholder="Example: 2031" />
+                                        <CustomInput control={form.control} name="state" label="State" placeholder="Example: NY" />
+                                        <CustomInput control={form.control} name="postalCode" label="Postal Code" placeholder="Example: 10101" />
                                     </div>
 
                                     <div className="flex gap-4">
                                         <CustomInput control={form.control} name="dateOfBirth" label="Date of Birth" placeholder="YYYY-MM-DD" />
-                                        <CustomInput control={form.control} name="ssn" label="Identity Number/SSN" placeholder="Enter your ID/SSN" />
+                                        <CustomInput control={form.control} name="ssn" label="SSN" placeholder="Example: 1234" />
                                     </div>
                                 </>
                             )}
@@ -161,7 +176,7 @@ const AuthForm = ({ type }: { type: string }) => {
                     </Link>
                 </footer>
           </>  
-        )}
+        )} 
     </section>
   )
 }
